5debaeed7a31c23a53a90c4bbf13ff0d554d9b87
Yoonyoung Cho <jchocholate@gmail.com>
2021-07-23 13:31:32 +0900

    try to be better

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-function list-dotfiles(){
+list_dotfiles(){
+    # NOTE(ycho): Find repository in a somewhat robust manner.
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    DIR="$(readlink -f $DIR)"
+
     #find . -maxdepth 1 -type f -name '\.*' -exec sh -c '
     #for f do
     #    git check-ignore -q "$f" || echo $f
     #done
     #' find-sh {} +
-    echo ".bashrc"
-    echo ".vimrc"
-    echo ".inputrc"
-    echo ".python_startup.py"
+
+    echo "${DIR}/.bashrc"
+    echo "${DIR}/.vimrc"
+    echo "${DIR}/.inputrc"
+    echo "${DIR}/.python_startup.py"
 }
 
-for src in $(list-dotfiles); do
+for src in $(list_dotfiles); do
     dst="$HOME/$(basename $src)"
     if [ -f "$dst" ]; then
         rm -i "$dst"
